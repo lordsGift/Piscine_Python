cfd2a18437d1611652f6c0f589e670dccca6716b
Matteo Marinelli <mmarinel@e4r7p2.42roma.it>
2022-05-19 14:49:49 +0200

    ex05 finalized

--- week1/ex05/ex05.py
+++ week1/ex05/ex05.py
@@ -6,7 +6,7 @@
 #    By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/18 22:58:10 by mmarinel          #+#    #+#              #
-#    Updated: 2022/05/19 14:23:48 by mmarinel         ###   ########.fr        #
+#    Updated: 2022/05/19 14:48:57 by mmarinel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -28,12 +28,13 @@ capital_cities = {
 	"NJ": "Trenton",
 	"CO": "Denver"
 }
-# if len(sys.argv) != 2:
-# 	sys.stderr.write("Error: Wrong Number of arguments" + "\n")
-states_inverted = dict(reversed(sorted(states.items())))
-print (states_inverted)
 
-# elif not sys.argv[1] in capital_cities.keys() or not capital_cities[sys.argv[1]] in states.keys():
-# 	sys.stdout.write("Unknown state" + "\n")
-# else:
-# 	sys.stdout.write(capital_cities[states[sys.argv[1]]] + "\n")
+states_inverted = dict(map(as_reversed, list(states.items())))
+capital_cities_inverted = dict(map(as_reversed, list(capital_cities.items())))
+
+if len(sys.argv) != 2:
+	exit()
+elif not sys.argv[1] in capital_cities_inverted.keys() or not capital_cities_inverted[sys.argv[1]] in states_inverted.keys():
+	sys.stdout.write("Unknown state" + "\n")
+else:
+	sys.stdout.write(states_inverted[capital_cities_inverted[sys.argv[1]]] + "\n")
