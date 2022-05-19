53b63e6e13d52a6470e824bf9aa147ea553196da
Matteo Marinelli <mmarinel@e4r7p2.42roma.it>
2022-05-19 11:40:15 +0200

    aggiunto defaultdict in ex03, print da finalizzare

--- week1/ex02/ex02.py
+++ week1/ex02/ex02.py
@@ -1,30 +1,32 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ex01.py                                            :+:      :+:    :+:    #
+#    ex02.py                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/05/18 22:58:10 by mmarinel          #+#    #+#              #
-#    Updated: 2022/05/19 10:14:18 by mmarinel         ###   ########.fr        #
+#    Updated: 2022/05/19 10:43:25 by mmarinel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import sys
 from functools import reduce
 
-def to_secs(time):
-	index, t = time;
-	return (pow(60, index) * t)
+def as_type(acc, n):
+	return (str(acc) + str(n) + " has a type " + str(type(n)) + '\n')
 
-def sum(n, m):
-	return (n + m)
+def	my_var():
+	l = [
+		42,
+		"42",
+		"quarante-deux",
+		42.0,
+		True,
+		[42],
+		{42: 42},
+		(42,),
+		set()
+	]
+	print(reduce(as_type, l, "")[:-1])
 
-def as_int(n):
-	if (not n.isdigit()):
-		return (0)
-	return (int(n))
-
-if len(sys.argv) != 4:
-	print ("Error: Wrong number of arguments (3 expected)")
-print(reduce(sum, map(to_secs, list(enumerate(map(as_int, sys.argv[1:])))), 0))
+my_var()
---
+++ week1/ex03/ex03.py
@@ -0,0 +1,50 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ex03.py                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/05/18 22:58:10 by mmarinel          #+#    #+#              #
+#    Updated: 2022/05/19 11:38:53 by mmarinel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from functools import reduce
+from collections import defaultdict
+
+def as_reversed(couple):
+	# print (tuple(reversed(couple)))
+	return (tuple(reversed(couple)))
+
+def	create_and_print_dict():
+	d = [
+		('Hendrix' , '1942'),
+		('Allman' , '1946'),
+		('King' , '1925'),
+		('Clapton' , '1945'),
+		('Johnson' , '1911'), 
+		('Berry' , '1926'),
+		('Vaughan' , '1954'),
+		('Cooder' , '1947'),
+		('Page' , '1944'),
+		('Richards' , '1943'),
+		('Hammett' , '1962'),
+		('Cobain' , '1967'),
+		('Garcia' , '1942'),
+		('Beck' , '1944'),
+		('Santana' , '1947'),
+		('Ramone' , '1948'),
+		('White' , '1975'),
+		('Frusciante', '1970'),
+		('Thompson' , '1949'),
+		('Burton' , '1939')
+	]
+	musicians = map(as_reversed, d)
+	my_dict = defaultdict(list)
+	for k, v in musicians:
+		my_dict[k].append(v)
+	sorted(my_dict.items())
+	print(my_dict)
+
+create_and_print_dict()
